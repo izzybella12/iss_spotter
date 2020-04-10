5846ba53d524c54d6cd87d367727980175439ad3
Isabella Panagrosso <isabellapanagrosso15@gmail.com>
2020-04-10 00:38:56 +0000

    using asynch on iss app

---
+++ index.js
@@ -0,0 +1,11 @@
+const fetchMyIp = require('./iss');
+
+
+// fetchMyIp((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
