d902e3ef29a8e6b48515b7f3cc450d1822cb4c41
Isabella Panagrosso <isabellapanagrosso15@gmail.com>
2020-04-10 03:18:06 +0000

    Added asynchs and callbacks

--- iss.js
+++ iss.js
@@ -1,22 +1,69 @@
 const request = require('request');
 
-let urlIP = 'https://api.ipify.org?format=json';
+// const fetchMyIp = function(callback) {
+//   request('https://api.ipify.org?format=json', (error, response, body) => {
+//     if (error) {
+//       callback(error, null);
+//     }
+//     if (response.statusCode !== 200) {
+//       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+//       callback(Error(msg), null);
+//     } else {
+//       let jsonBody = JSON.parse(body);
+//       let ip = jsonBody.ip;
+//       callback(null, ip);
+//     }
+//   });
+// };
 
-const fetchMyIp = function(callback) {
-  request(urlIP, (error, response, body) => {
+// const fetchCoordsByIp = function(ip, callback) {
+//   request("https://ipvigilante.com/json/" + ip, (error, response, body) => {
+//     if (error) {
+//       callback(error, null);
+//     }
+//     if (response.statusCode !== 200) {
+//       const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+//       callback(Error(msg), null);
+//     } else {
+//       let jsonBody = JSON.parse(body);
+//       let latitude = jsonBody.data.latitude;
+//       let longitude = jsonBody.data.longitude;
+//       let coordinates = {latitude, longitude};
+//       callback(null, coordinates);
+//     }
+//   });
+// };
+
+const fetchISSFlyOverTimes = function(objectLatLong, callback) {
+  let urlISS = "http://api.open-notify.org/iss-pass.json?lat=" + objectLatLong.latitude + "&lon=" + objectLatLong.longitude;
+  request(urlISS , (error, response, body) => {
     if (error) {
       callback(error, null);
-    }
-    if (response.statusCode !== 200) {
-      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
-      callback(Error(msg), null);
       return;
-    } else {
-      let jsonBody = JSON.parse(body);
-      let ip = jsonBody.ip;
-      callback(null, ip);
     }
+    // if (response.status !== 200) {
+    //   const msg = `Status Code ${response.statusCode} when fetching coordinates for ISS. Response: ${body}`;
+    //   console.log(Error(msg), null)
+    //   return;
+    // }
+    let infoISS = JSON.parse(body);
+    let flyOverTimes = infoISS.response;
+    callback(null, flyOverTimes);
   });
 };
 
-module.exports = fetchMyIp;
+///// QUESTIONS:
+///// Why is it thinking that the response.status is not 200?
+///// Are we supposed to be taking away the callbacks and re-console.loging, in order to be
+///// calling the functions in other functions?
+
+////////////////////////////
+
+let nextISSTimesForMyLocation = function(callback) {
+  
+}
+
+// module.exports = fetchMyIp;
+// module.exports = fetchCoordsByIp;
+// module.exports = fetchISSFlyOverTimes;
+module.exports = nextISSTimesForMyLocation;
