6fcba6b71f2b8f7f358534a55f4e31d2db113db2
Isabella Panagrosso <isabellapanagrosso15@gmail.com>
2020-04-10 21:48:15 +0000

    final changes

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// const fetchMyIp = require('./iss');
-// const fetchCoordsByIp = require('./iss');
-// const fetchISSFlyOverTimes = require('./iss');
 const nextISSTimesForMyLocation = require('./iss');
 
 // fetchMyIp((error, ip) => {
@@ -12,33 +9,38 @@ const nextISSTimesForMyLocation = require('./iss');
 //   }
 // });
 
-// fetchCoordsByIp('32.212.164.168', (error, data) => {
+// fetchCoordsByIp('32.212.164.168', (error, coordsIP) => {
 //   if (error) {
 //     console.log("It didn't work!" , error);
 //     return;
 //   } else {
-//     console.log('It worked! Returned IP:' , data);
+//     console.log('It worked! Returned IP:' , coordsIP);
 //   }
 // });
 
 // let exampleCoords = { latitude: '41.27940', longitude: '-73.03290' };
-// fetchISSFlyOverTimes(exampleCoords, (error, data) => {
+// fetchISSFlyOverTimes(exampleCoords, (error, coords) => {
 //   if (error) {
 //     console.log("It didn't work!" , error);
 //     return;
 //   } else {
-//     console.log('It worked! Returned ISS Spots:' , data);
+//     console.log('It worked! Returned ISS Spots:' , coords);
 //   }
 // });
 
-nextISSTimesForMyLocation((error, passTimes) => {
+let printPassTimes = function(flyOverTimes) {
+  for (const pass of flyOverTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, flyOverTimes) => {
   if (error) {
     return console.log("It didn't work!", error);
   } else {
-    console.log(passTimes);
+    printPassTimes(flyOverTimes);
   }
 });
-
-
-///// Questions: 
-///// WHy is there a return before the console.log in the nextISSTimesForMyLocation called function?
